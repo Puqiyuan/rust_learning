8059e7765a5334e80da837f35c0951ec9a54a5f4
Qiyuan Pu <pqy7172@gmail.com>
2021-06-26 23:59:46 +0800

    add fisrt learning program

---
+++ codes/target/debug/codes.d
@@ -0,0 +1,1 @@
+/home/pqy7172/rust_learning/codes/target/debug/codes: /home/pqy7172/rust_learning/codes/src/main.rs
---
+++ codes/target/debug/deps/codes-640e510d21e87e16.d
@@ -0,0 +1,5 @@
+/home/pqy7172/rust_learning/codes/target/debug/deps/codes-640e510d21e87e16: src/main.rs
+
+/home/pqy7172/rust_learning/codes/target/debug/deps/codes-640e510d21e87e16.d: src/main.rs
+
+src/main.rs:
